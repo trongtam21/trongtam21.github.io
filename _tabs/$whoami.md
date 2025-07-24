c79c13388906edc5f7645c40e7f6e5dfa188fe6b
trongtam21 <dotrongtam4321@gmail.com>
2025-07-24 17:53:39 +0700

    1

---
+++ _tabs/$whoami.md
@@ -0,0 +1,8 @@
+---
+# the default layout is 'page'
+icon: fas fa-info-circle
+order: 4
+---
+
+## About Me
+My name is Trọng Tâm, and I am currently a student at the Academy of Cryptography Techniques. My primary focus is on researching cybersecurity, a field that deeply interests me. In addition to my studies, I actively participate in CTF (Capture The Flag) competitions during my free time, which allows me to further sharpen my skills and stay updated with the latest trends and challenges in the cybersecurity world.
---
+++ burrow/sus.js
@@ -0,0 +1,36 @@
+(function() {
+    const petyaArt = `
+               uu$$$$$$$$$$$uu
+            uu$$$$$$$$$$$$$$$$$uu
+           u$$$$$$$$$$$$$$$$$$$$$u
+          u$$$$$$$$$$$$$$$$$$$$$$$u
+         u$$$$$$$$$$$$$$$$$$$$$$$$$u
+         u$$$$$$*   *$$$*   *$$$$$$u
+         *$$$$*      u$u       $$$$*
+          $$$u       u$u       u$$$
+          $$$u      u$$$u      u$$$
+           *$$$$uu$$$   $$$uu$$$$*
+            *$$$$$$$*   *$$$$$$$*
+              u$$$$$$$u$$$$$$$u
+               u$*$*$*$*$*$*$u
+    uuu        $$u$ $ $ $ $u$$       uuu
+    u$$$$       $$$$$u$u$u$$$       u$$$$
+    $$$$$uu      *$$$$$$$$$*     uu$$$$$$
+  u$$$$$$$$$$$uu    *****    uuuu$$$$$$$$$
+  $$$$***$$$$$$$$$$uuu   uu$$$$$$$$$***$$$*
+   ***      **$$$$$$$$$$$uu **$***
+            uuuu **$$$$$$$$$$uuu
+   u$$$uuu$$$$$$$$$uu **$$$$$$$$$$$uuu$$$
+   $$$$$$$$$$****           **$$$$$$$$$$$*
+     *$$$$$*                      **$$$$**
+       $$$*                         $$$$*
+  `;
+  
+    const uniqueID = "M2RfMW5fb1VyX3A0ZzN9";
+  
+    console.log("%c[ You have been pwned ]", "color: red; font-weight: bold; font-size: 16px;");
+    console.log("Flag 2: %c" + uniqueID, "color: yellow; font-weight: bold;");
+    console.log("Looking for the first flag part? Maybe you should check your code ( •̀ᴗ•́ )");
+    console.log(petyaArt);
+  })();
+  
